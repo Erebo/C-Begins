a795d7118adfb61998592f379066801b0b54dcf3
MetroclesTThedoreF <rahmanmahadi177153@gmail.com>
2025-05-17 12:03:16 +0600

    N/A

---
+++ getcharAndputcahr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char character;
+    printf("Enter your character : ");
+    character = getchar();
+    printf("You entered : ");
+    putchar(character);
+
+
+
+
+
+    return 0; 
+}
