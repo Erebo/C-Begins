bbe983983d95c5d53cc0c8908b27bd225ee712f7
MetroclesTThedoreF <rahmanmahadi177153@gmail.com>
2025-05-18 00:16:41 +0600

    N/A

---
+++ AsciitoCharAndChartoAscii.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int n; 
+char m;
+printf("Enter a ASCII number : ");
+scanf("%d",&n);
+printf("Character of the ASCII code is : %c\n",n);
+printf("Enter a character : ");
+scanf("%s",&m);
+printf("ASCII value of the Character is : %d",m);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
