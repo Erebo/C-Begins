c04a2114a69b92a6d717e9fe883185328b1ad846
MetroclesTThedoreF <rahmanmahadi177153@gmail.com>
2025-05-22 16:38:48 +0600

    ''is must

--- IfElsePractice.c/SmallCapitalLetter.c
+++ IfElsePractice.c/SmallCapitalLetter.c
@@ -2,8 +2,8 @@
 int main(){
     char letter;
     printf("Enter a letter : ");
-    scanf("%c",&letter);
-    if(letter>='a' && letter<='z'){
+    scanf("%c",&letter);// just using >= a is not enough it will iclude all the symbols that are not character 
+    if(letter>='a' && letter<='z'){//<= z contatins all the character and exclude all the symbols 
         printf("It's in small letter ");
     } else if (letter>='A' && letter<='Z'){
         printf("It's in Capital letter");
