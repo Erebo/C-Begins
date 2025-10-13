4b71e29d1f72245592e190f541beb3c2ff2d462b
Erebo <rahmanmahadi177153@gmail.com>
2025-10-13 15:09:46 +0600

    Deletation of last node

---
+++ Data Structure/LinkedList/Deletation/Last.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct student {
+    int data;
+    struct student *next;
+};
+struct student *head=NULL;
+void print(){
+    struct student *i=head;
+    while(i != NULL){
+        printf("%d->",i->data);
+        i=i->next;
+    }
+    printf("NULL\n");
+}
+void createNewnode(){
+    int x;
+    printf("Enter Data : ");
+    scanf("%d",&x);
+    struct student *newnode;
+    newnode=(struct student*)malloc(sizeof(struct student));
+    newnode->data=x;
+    newnode->next=NULL;
+    if(head == NULL){
+        head = newnode;
+    } else{
+        struct student *i=head;
+        while(i->next != NULL){
+            i=i->next;
+        }
+        i->next=newnode;
+    }
+}
+void deleteLast(){
+    if(head == NULL){
+        printf("There's nothing to delete.");
+    } else if(head->next==NULL){
+        head=NULL;
+    } else{
+    struct student*i=head;
+    while(i->next->next != NULL){
+        i=i->next;
+    }
+    i->next=NULL;
+}
+}
+int main(){
+    /*createNewnode();
+    createNewnode();
+    createNewnode();*/
+    createNewnode();
+    print();
+    deleteLast();
+    print();
+    return 0;
+}
